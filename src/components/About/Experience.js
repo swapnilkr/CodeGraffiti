55a8f9456817f8ab2470844be2cedfb71962bb1e
swapnilkr099 <swapnilkr099@gmail.com>
2023-10-11 23:44:19 +0530

    Experience section

--- src/components/About/Experience.js
+++ src/components/About/Experience.js
@@ -3,104 +3,82 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particles";
 
 
-const Experience = () => {
+function Experience() {
+
+  const experienceList = [
+    {
+      id: 1,
+      title: 'Associate Software Developer',
+      company: 'Motorola Mobility - Lenovo',
+      description: 'Re-engineered a legacy third-party software system, introducing a custom user grievance reporting flow.
+      Engineered a dynamic cloud-based user segmentation framework for personalized engagement.
+      Pioneered the development of a Google Dialogflow-powered Gitbot, enabling frictionless purchases.
+      Spearheaded a 50% reduction in menu page load times.
+      Orchestrated the deployment of an adaptive user segmentation banner for targeted promotions and surveys.
+      Standardized and harmonized design elements across an extensive portfolio of 623 UI screens.
+      Seamlessly delivered beta, soft, and prod launches to 11 million devices.
+      Orchestrated the implementation of a cloud-delivered dialog interface for sophisticated push notifications.',
+      imageUrl: 'experience1.jpg', // Replace with your image URL
+      startDate: 'idk',
+      endDate: 'idk'
+    },
+    {
+      id: 1,
+      title: 'sd;lkl;sd',
+      company: 'ABC Tech',
+      description: 'Worked on creating user-friendly and responsive web applications using React, HTML, CSS, and JavaScript.',
+      imageUrl: 'experience1.jpg', // Replace with your image URL
+      startDate: 'idk',
+      endDate: 'idk'
+    },
+    {
+      id: 1,
+      title: 'Front-end Developer',
+      company: 'ABC Tech',
+      description: 'Worked on creating user-friendly and responsive web applications using React, HTML, CSS, and JavaScript.',
+      imageUrl: 'experience1.jpg', // Replace with your image URL
+      startDate: 'idk',
+      endDate: 'idk'
+    },
+
+  ]
+
   return (
     <Container fluid className="experience-section">
       <Particle />
       <Row>
-        <section id="experience" style={{justifyContent: "center"}}>
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
-               <strong className="purple">Experience</strong>
-            </h1>
+        <section id="experience" style={{ justifyContent: "center" }}>
+          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
+            <strong className="purple">Experience</strong>
+          </h1>
 
           <div class="timeline">
-            <div class="timeline-box">
-              <div class="timeline-conatiner">
-                <div class="timeline-logo">
-                  <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000030_1550710829_ninjasicon.png" />
+            {experienceList.map((key, index) => (
+              <div class="timeline-box">
+                <div class="timeline-conatiner">
+                  <div class="timeline-logo">
+                    <img src={key.imageUrl} />
+                  </div>
+                  <h3 class="experience-designation m0 m-blue"> &nbsp; &nbsp; &nbsp;
+                    {key.title}
+                  </h3>
+                  <h4 class="experience-company-name">
+                    {key.company}
+                  </h4>
+                  <h5 class="experience-duration m0">
+                    {key?.startDate} - {key?.endDate}</h5>
+                  <p class="experience-description text-align-justify">
+                    {key?.description}
+                  </p>
                 </div>
-                <h3 class="experience-designation m0 m-blue"> &nbsp; &nbsp; &nbsp;Head Co-ordinator </h3>
-                <h4 class="experience-company-name">College Event</h4>
-                <h5 class="experience-duration m0">
-                  Aug 2019 </h5>
-                <p class="experience-description text-align-justify">
-                  Head Co-ordinator, Lakshya Event: Science Project & Model Making – Led 5 coordinators & managed 45+ volunteers for smooth functioning of 3-day long event.
-                </p>
               </div>
-            </div>
-            <div class="timeline-box right">
-              <div class="timeline-conatiner">
-                <div class="timeline-logo">
-                  <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000030_1550710829_ninjasicon.png" />
-                </div>
-                <h3 class="experience-designation m0 m-blue"> Class Representative </h3>
-                <h4 class="experience-company-name">Dept. Of Computer Engineering</h4>
-                <h5 class="experience-duration m0">
-                  Aug 2018 - Present </h5>
-                <p class="experience-description text-align-justify">
-                  Unanimously elected to led & represent the batch of 120 students in the department
-                  Attend various General Body Meetings to lay grievances of students
-                </p>
-              </div>
-            </div>
-
-
-            <div class="timeline-box left">
-              <div class="timeline-conatiner">
-                <div class="timeline-logo">
-                  <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000030_1550710829_ninjasicon.png" />
-                </div>
-                <h3 class="experience-designation m0 m-blue"> &nbsp; &nbsp; &nbsp; Virtual Experience </h3>
-                <h4 class="experience-company-name">Microsoft</h4>
-                <h5 class="experience-duration m0">
-                  July 2020 </h5>
-                <p class="experience-description text-align-justify">
-                  Completed 6 modules on various skillsets like Virtual Experience , Business Foundation skill , Campus to Corporate : Work Ready skill and Technical Engineer Skills.
-                </p>
-              </div>
-            </div>
-
-            <div class="timeline-box right">
-              <div class="timeline-conatiner">
-                <div class="timeline-logo">
-                  <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000030_1550710829_ninjasicon.png" />
-                </div>
-                <h3 class="experience-designation m0 m-blue">&nbsp; &nbsp; &nbsp; Virtual  Experience </h3>
-                <h4 class="experience-company-name">JPMorgan Chase & co.</h4>
-                <h5 class="experience-duration m0">
-                  June 2020 - Jul 2020 </h5>
-                <p class="experience-description text-align-justify">
-                  Establishing Financial Data Feed – Fixed broken datafeed script, created automated time-interval generated data & developed git patch.
-                </p>
-              </div>
-            </div>
-
-            <div class="timeline-box left">
-              <div class="timeline-conatiner">
-                <div class="timeline-logo">
-                  <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000030_1550710829_ninjasicon.png" />
-                </div>
-                <h3 class="experience-designation m0 m-blue"> Internship </h3>
-                <h4 class="experience-company-name">Internship-Studio</h4>
-                <h5 class="experience-duration m0">
-                  Aug 2020 - present</h5>
-                <p class="experience-description text-align-justify">
-                  Machine Learning Based project.
-                </p>
-              </div>
-            </div>
-
+            ))}
             <div class="timeline-divider plane">
               <div class="timeline-traveller">
-
                 <i class="fas fa-plane"></i>
-
-
               </div>
             </div>
-
           </div>
-
         </section>
 
       </Row>
