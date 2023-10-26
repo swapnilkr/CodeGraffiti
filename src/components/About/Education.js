a57982ab662ca88d2177a77db1460d9028a39907
swapnilkr099 <swapnilkr099@gmail.com>
2023-10-27 02:21:56 +0530

    Done education block

--- src/components/About/Education.js
+++ src/components/About/Education.js
@@ -1,103 +1,93 @@
 import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particles";
-import Moto from '../../Assets/moto.png'
-import Sparks from '../../Assets/Sparks.png'
-import Softora from "../../Assets/Softora.png"
-import CodeSpeedy from "../../Assets/CodeSpeedy.png"
-import TCS from "../../Assets/TCS.png"
-import IStudio from "../../Assets/ISudtio.png"
-import {BsFillAirplaneFill} from "react-icons/bs";
+import Sapthagiri from '../../Assets/Sap.jpg'
+import Divine from '../../Assets/divine.png'
+import Joseph from '../../Assets/Joseph.png'
+
+
+import { AiFillCar } from "react-icons/ai";
 
 
 function Education() {
 
-  const educationList = [
-    {
-      id: 1,
-      title: 'Associate Software Developer',
-      company: 'Motorola Mobility - Lenovo',
-      description: `Re-engineered legacy third-party software system, yielding annual savings of <span class="purple">320K</span> INR.
-      Managed successful launches to <span class="purple">11 million</span> devices.
-      Standardized design elements across <span class="purple">623 UI </span> screens.
-      Pioneered Google <span class="purple">Dialogflow-powered Giftbot</span> enabling frictionless purchases.
-      Attained <span class="purple">50%</span> reduction in menu page load times.
-      Deployed dynamic user segmentation banner for precise promotions and surveys.
-      Implemented <span class="purple">cloud-based dialog</span> interface for streamlined push notifications.`,
-      imageUrl: Moto,
-      startDate: 'Aug 2023',
-      endDate: 'Present'
-    },
-    {
-      id: 2,
-      title: 'Intern',
-      company: 'Sparks Foundation',
-      description: `Integrated <span class="purple">Google OAuth2 </span> for advanced user login, enhancing security and user convenience.
-      Significantly improved user auth, leading to heightened security and seamless login experience.`,
-      imageUrl: Sparks,
-      startDate: 'July 2021',
-      endDate: 'Aug 2021'
-    },
-    {
-      id: 3,
-      title: 'Intern',
-      company: 'Softora Tech Pvt Ltd',
-      description: `Created "Pockket," a dynamic <span class="purple">Node.js</span>-based payment gateway with MongoDB integration.
-      Implemented Passport for local and Google OAuth2 for user authentication and authorization.
-      Utilized MessageBird API for OTP verification.`,
-      imageUrl: Softora,
-      startDate: 'Apr 2021',
-      endDate: 'May 2021'
-    },
-  ]
+    const educationList = [
+        {
+            id: 1,
+            title: 'B.E. in CSE',
+            company: 'Sapthagiri College of Engineering',
+            description: `Cumulative Grade Point Average (CGPA): <span class="purple">8.97</span>/10.0 .
+            Consistently ranked in the top 3% of the academic batch.`,
+            imageUrl: Sapthagiri,
+            endDate: 'Batch of 2022'
+        },
+        {
+            id: 2,
+            title: 'Intermediate College- CBSE',
+            company: 'Divine Happy School',
+            description: `Passed with 70.60%`,
+            imageUrl: Divine,
+            endDate: 'Batch of 2017'
+        },
+        {
+            id: 3,
+            title: 'Matriculation- ICSE',
+            company: `St. Joseph's School`,
+            description: `Passed with <span class="purple">93 %</span>.
+            Awarded With best speaker of Batch.`,
+            imageUrl: Joseph,
+            startDate: 'Apr 2021',
+            endDate: 'May 2021'
+        },
+    ]
 
 
-  return (
-    <Container fluid className="experience-section">
-      <Particle />
-      <Row>
-        <section id="experience" style={{ justifyContent: "center" }}>
-          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
-            <strong className="purple">Education</strong>
-          </h1>
+    return (
+        <Container fluid className="experience-section">
+            <Particle />
+            <Row>
+                <section id="experience" style={{ justifyContent: "center" }}>
+                    <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
+                        <strong className="purple">Education</strong>
+                    </h1>
 
-          <div class="timeline">
-            {educationList.map((key, index) => (
-              <div class="timeline-box">
-                <div class="timeline-conatiner">
-                  <div class="timeline-logo">
-                    <img style={{ borderRadius: (index === 1 || index === 2 || index === 5 || index === 6  ? '20px' : '') }} src={key.imageUrl} />
-                  </div>
-                  <h3 class="experience-designation m0 m-blue"> &nbsp; &nbsp; &nbsp;
-                    {key.title}
-                  </h3>
-                  <h4 class="experience-company-name">
-                    {key.company}
-                  </h4>
-                  <h5 class="experience-duration m0">
-                    {key?.startDate} - {key?.endDate}</h5>
-                  <ul class="experience-description text-align-justify">
-                    {key.description.split('\n').map((point, i) => (
-                      <li key={i}>
-                        <span dangerouslySetInnerHTML={{ __html: point }} />
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            ))}
-            <div class="timeline-divider plane">
-              <div class="timeline-traveller">
-                <BsFillAirplaneFill />
-              </div>
-            </div>
-          </div>
-        </section>
+                    <div class="timeline">
+                        {educationList.map((key, index) => (
+                            <div class="timeline-box">
+                                <div class="timeline-conatiner">
+                                    <div class="timeline-logo">
+                                        <img style={{ borderRadius: (index === 1 || index === 2 ? '20px' : '') }} src={key.imageUrl} />
+                                    </div>
+                                    <h3 class="experience-designation m0 m-blue"> &nbsp; &nbsp; &nbsp;
+                                        {key.title}
+                                    </h3>
+                                    <h4 class="experience-company-name">
+                                        {key.company}
+                                    </h4>
+                                    <h5 class="experience-duration m0">
+                                        {key?.endDate}</h5>
+                                    <ul class="experience-description text-align-justify">
+                                        {key.description.split('\n').map((point, i) => (
+                                            <li key={i}>
+                                                <span dangerouslySetInnerHTML={{ __html: point }} />
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        ))}
+                        <div class="timeline-divider plane">
+                            <div class="timeline-traveller car">
+                                <AiFillCar />
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
-      </Row>
-    </Container>
+            </Row>
+        </Container>
 
-  )
+    )
 
 }
 
