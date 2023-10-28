a95db8cbb6b02068f25e58bcdd7d9fb36cac5f01
swapnilkr099 <swapnilkr099@gmail.com>
2023-10-28 23:47:23 +0530

    Skillset anim

--- src/components/About/Skillset.js
+++ src/components/About/Skillset.js
@@ -27,6 +27,7 @@ function Skillset() {
         <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
             <Col xs={4} md={2} className="tech-icons">
                 <DiReact />
+                {/* <span className="tech-name">React</span> */}
             </Col>
             <Col xs={4} md={2} className="tech-icons">
                 <DiJavascript1 />
