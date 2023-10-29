43a450477a71e2568b6b5c096be7634953cbe8cb
swapnilkr099 <swapnilkr099@gmail.com>
2023-10-30 01:07:43 +0530

    Tollset animation

--- src/components/About/Toolset.js
+++ src/components/About/Toolset.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
     SiVisualstudiocode,
@@ -15,22 +15,54 @@ import {
 
 
 function Toolstack() {
+    const [isLogoVisible, setIsLogoVisible] = useState(true);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsLogoVisible((prev) => !prev);
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [])
+
     return (
         <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
             <Col xs={4} md={2} className="tech-icons">
-                <SiVisualstudiocode />
+                {isLogoVisible ?
+                    <SiVisualstudiocode className="skillset-logo fadeinoutLogo" />
+                    :
+                    <span className="tech-name fadeinoutName">VS Code</span>
+                }
             </Col>
             <Col xs={4} md={2} className="tech-icons">
-                <SiJupyter />
+                {isLogoVisible ?
+                    <SiJupyter className="skillset-logo fadeinoutLogo" />
+                    :
+                    <span className="tech-name fadeinoutName">Jupyter</span>
+                }
             </Col>
             <Col xs={4} md={2} className="tech-icons">
-                <SiPostman />
+                {isLogoVisible ?
+                    <SiPostman className="skillset-logo fadeinoutLogo" />
+                    :
+                    <span className="tech-name fadeinoutName">Postman</span>
+                }
             </Col>
             <Col xs={4} md={2} className="tech-icons">
-                <ImNpm />
+                {isLogoVisible ?
+                    <ImNpm className="skillset-logo fadeinoutLogo" />
+                    :
+                    <span className="tech-name fadeinoutName">NPM</span>
+                }
             </Col>
             <Col xs={4} md={2} className="tech-icons">
-                <DiGulp />
+                {isLogoVisible ?
+                    <DiGulp className="skillset-logo fadeinoutLogo" />
+                    :
+                    <span className="tech-name fadeinoutName">Gulp</span>
+                }
             </Col>
         </Row>
     );
