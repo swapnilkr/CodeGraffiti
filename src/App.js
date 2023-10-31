3a9ac505200a0710808adb29bb0b510264aa6dc6
swapnilkr099 <swapnilkr099@gmail.com>
2023-10-31 22:51:25 +0530

    Indentation

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Whoiam from "./components/About/Whoiam.js";
 import Experience from "./components/About/Experience.js";
 import Footer from "./components/Footer";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
@@ -18,33 +18,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
-  const [startup, setStartup] = useState(true);
+	const [startup, setStartup] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setStartup(false);
-    }, 3000);
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setStartup(false);
+		}, 3000);
 
-    return () => clearTimeout(timeout);
-  }, []);
+		return () => clearTimeout(timeout);
+	}, []);
 
 
-  return (
+	return (
 
-    <Router>
-      <Preloader startup={startup} />
-      <NavBar />
-      <Routes>
-          <Route path="/" element={<HomeTSA />} />
-          <Route path="/whoiam" element={<Whoiam />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-      </Routes>
-      <ScrollToTop />
-      <Footer />
-    </Router>
+		<Router>
+			<Preloader startup={startup} />
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<HomeTSA />} />
+				<Route path="/whoiam" element={<Whoiam />} />
+				<Route path="/experience" element={<Experience />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+			<ScrollToTop />
+			<Footer />
+		</Router>
 
-  );
+	);
 }
 
 export default App;
